2f89dc69bc60a52de41f87b044dfca9314fd1c04
KienNT <kiennt7902@gmail.com>
2024-06-28 13:41:44 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// arrow function: () => {}
 
-function App() {
+//Default function
+// function App() {
+//   const [count, setCount] = useState(0)
+
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Hello Tibbreat</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+//Arrow function
+const App = () => {
   const [count, setCount] = useState(0)
 
+  const arrowFunction = () => {
+    console.log(">> my arrow function")
+  }
+
+  arrowFunction();
   return (
     <>
       <div>
@@ -33,3 +74,4 @@ function App() {
 }
 
 export default App
+
