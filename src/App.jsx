d5df76a5eb27b400b6f9f617338702dc2adfe35d
KienNT <kiennt7902@gmail.com>
2024-06-28 13:55:32 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// arrow function: () => {}
-
-//Default function
-// function App() {
-//   const [count, setCount] = useState(0)
 
 
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Hello Tibbreat</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
+//Tên component phải viết hoa chữ cái đầu.
+//component = html + css + js
+//component phải return về html
+const MyComponent = () => {
+  return (
+    <div> Tibbreat </div>
+  );
+}
 
 
-//Arrow function
-const App = () => {
+function App() {
   const [count, setCount] = useState(0)
 
-  const arrowFunction = () => {
-    console.log(">> my arrow function")
-  }
-
-  arrowFunction();
   return (
     <>
       <div>
@@ -58,6 +28,7 @@ const App = () => {
         </a>
       </div>
       <h1>Hello Tibbreat</h1>
+      <MyComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -74,4 +45,3 @@ const App = () => {
 }
 
 export default App
-
